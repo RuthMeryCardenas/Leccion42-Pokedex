18a041b680e8f96f67a7a3f1e4bda67e88f199d2
RuthMery <ruth.cardenas.perez@gmail.com>
2017-06-19 09:14:14 -0500

    results-Cambiando estructura y estilos

--- js/components/results.js
+++ js/components/results.js
@@ -4,7 +4,9 @@ const reRender = (element,filter,update) => {
   const filteredPokemons = filterByName(state.pokemons,filter);
   if (filteredPokemons.length > 0) {
     $.each(filteredPokemons,(index,pokemon) => {
-      element.append(Item_pokemon(pokemon,update));
+      const col = $("<div class='col l2'></div>");
+      col.append(Item_pokemon(pokemon));
+      element.append(col);
     });
   } else {
     element.append($('<p>Pokemon no encontrado</p>'));
@@ -16,41 +18,30 @@ const Results = (update) => {
   return results;
 }
 const List_pokemon = (pokemons, update) => {
-  const list_pokemon = $("<div class='list-pokemon'></div>");
-  var aux = 1;
-  var aux_col = [];
+  const list_pokemon = $("<div class='row list-pokemon'></div>");
   pokemons.forEach ((pokemon, index) => {
-    aux_col.push(Item_pokemon(pokemon));
-    if (aux % 5 == 0) {
-      const row = $("<div class='row'></div>");
-      const col = $("<div class='col s10 offset-s1'></div>");
-      for (var i = 0; i < aux_col.length; i++) {
-        col.append($(aux_col[i]));
-      }
-      aux_col = [];
-      row.append(col);
-      list_pokemon.append(row);
-    }
-    aux ++
-  });
+    const col = $("<div class='col l2'></div>");
+    col.append(Item_pokemon(pokemon));
+    list_pokemon.append(col);
+    });
   return list_pokemon;
 }
 const Item_pokemon = (pokemon) => {
-  const item_pokemon = $("<div class='item-pokemon grey lighten-2'></div>");
-  const container_img = $("<div class='img-pokemon'></div>");
-  const img_pokemon = $("<img src='http://serebii.net/art/th/" + (pokemon.entry_number) + ".png' alt='' class='responsive-img'></img>");
+  const item_pokemon = $("<div class='item-pokemon grey lighten-3'></div>");
+  const container_img = $("<div class='container-img'></div>");
+  const img_pokemon = $("<img src='http://serebii.net/art/th/" + (pokemon.entry_number) + ".png' alt=''></img>");
   const trapeze = $("<div class='trapeze'></div>");
 
   container_img.append(img_pokemon);
   item_pokemon.append(container_img);
   item_pokemon.append(trapeze);
-  item_pokemon.append(User_action(pokemon.pokemon_species.name));
+  item_pokemon.append(User_action(pokemon.entry_number, pokemon.pokemon_species.name));
   return item_pokemon;
 }
-const User_action = (name_pokemon, update) => {
+const User_action = (order_pokemon, name_pokemon, update) => {
   const user_action = $("<div class='user-action'></div>");
   const container_icons = $("<div class='container-icons center-align'></div>");
-  const link_1 = $("<a href='#'></a>");
+  const link_1 = $("<a href='#' data-order=" + order_pokemon + "></a>");
   const icon_1 = $("<img src='assets/icon/pokeball_gray.png' alt=''></img>");
   const link_2 = $("<a href='#'></a>");
   const icon_2 = $("<img src='assets/icon/valentines-heart.png' alt=''></img>");
